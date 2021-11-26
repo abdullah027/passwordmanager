9633bcc304801b138efc1787291705eba00bf78d
Muhammad Abdullah <79091197+neon027@users.noreply.github.com>
2021-11-26 12:52:05 +0500

    updated

---
+++ lib/utilis/image_picker.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+
+class PickFile {
+  Future<File?> getImage() async {
+    File file;
+
+    ImagePicker filePicker = ImagePicker();
+
+    final pickedFile = await filePicker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      file = File(pickedFile.path);
+      return file;
+    } else {
+      print('No image selected.');
+      return null;
+    }
+  }
+
+  Future<File?> pickAVideo() async {
+    File file;
+
+    ImagePicker filePicker = ImagePicker();
+
+    final pickedFile = await filePicker.pickVideo(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      print(pickedFile.path);
+      file = File(pickedFile.path);
+      return file;
+    } else {
+      print('No Video selected.');
+      return null;
+    }
+  }
+}
